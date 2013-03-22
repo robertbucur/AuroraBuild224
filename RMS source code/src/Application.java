15c397bbd452c0440bc22f4c97a5f5262d1c1661
KosmynMDC <cosmin.mihu@gmail.com>
2013-03-22 02:28:03 +0200

    Initial project structure

---
+++ RMS source code/src/Application.java
@@ -0,0 +1,19 @@
+/**
+ * Entry point
+ *
+ * @author Mihu Dumitru-Cosmin
+ * @version 1.0, Mar 22, 2013
+ */
+public class Application {
+
+	/**
+	 * Main Method
+	 * 
+	 * @param args
+	 * @since version 1.0
+	 */
+	public static void main(String[] args) {
+		Application app = new Application();
+	}
+	
+}
