1df12b8b1ea80c36946e72c0fd4d4e579ba2cac6
AndreiTomas <andrei_volei1@yahoo.com>
2013-04-16 21:20:26 +0300

    [BusinessLogic-AndreiTomas] Clasa Controller

---
+++ RMS source code/src/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrei
+ * Date: 4/16/13
+ * Time: 9:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+}
