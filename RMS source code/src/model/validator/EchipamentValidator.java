08373dd7905c4f1e112c722fe312c065f73ef6ec
Andreea <andreutza_3105@yahoo.com>
2013-04-24 22:50:30 +0300

    [Business Logic] metodele add finalizate + update ceva

---
+++ RMS source code/src/model/validator/EchipamentValidator.java
@@ -0,0 +1,10 @@
+package model.validator;
+
+
+public class EchipamentValidator implements Validator {
+    @Override
+    public boolean validate(Object o) {
+        //TODO: trebe implementat
+        return true;
+    }
+}
---
+++ RMS source code/src/model/validator/SalaValidator.java
@@ -0,0 +1,10 @@
+package model.validator;
+
+
+public class SalaValidator implements Validator{
+    @Override
+    public boolean validate(Object o) {
+        //TODO: trebe implementat
+        return true;
+    }
+}
